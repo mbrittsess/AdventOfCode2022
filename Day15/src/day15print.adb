b2e18060c32045f6bcbbcd4a792615fd55a147fc
msessions <msessions@beckettcorp.com>
2022-12-19 16:43:17 -0500

    Added simple print-program

---
+++ Day15/src/day15print.adb
@@ -0,0 +1,30 @@
+with Tunnels; use Tunnels;
+with Sensors;
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Day15Print is
+   Output_Char : array ( Tunnels.Content ) of Character :=
+     (
+      Tunnels.Empty => '.',
+      Tunnels.Sensor => 'S',
+      Tunnels.Beacon => 'B'
+     );
+   
+begin
+   for Y in Sensors.Min_Pos.Y .. Sensors.Max_Pos.Y loop
+      Put( Y'Image & ": " );
+      for X in Sensors.Min_Pos.X .. Sensors.Max_Pos.Y loop
+         declare
+            Content : Tunnels.Content := Tunnels.Get_Content(X,Y);
+         begin
+            if Content = Tunnels.Empty and then Tunnels.Definitely_Not_Beacon(X,Y) then
+               Put( '#' );
+            else
+               Put( Output_Char( Content ) );
+            end if;
+         end;
+      end loop;
+      New_Line;
+   end loop;
+   
+end Day15Print;
