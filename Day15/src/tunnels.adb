232820ced1ce3c429e0f6115d3cf163c50661d03
msessions <msessions@beckettcorp.com>
2022-12-19 14:35:35 -0500

    Completed day 15, part 1

--- Day15/src/tunnels.adb
+++ Day15/src/tunnels.adb
@@ -2,46 +2,8 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 package body Tunnels is
    
-   function Get_Content ( X, Y : Integer ) return Content is (Area(X,Y));
+   function Get_Content ( Pos : Position ) return Content is (if (for some Info of All_Sensors => Pos = Info.Sensor) then Sensor elsif (for some Info of All_Sensors => Pos = Info.Beacon) then Beacon else Empty);
    
-   function Definitely_Not_Beacon ( X, Y : Integer ) return Boolean is (Beacon_Status(X,Y));
-
-begin
-   
-   --Put_Line( Beacon_Status'First(1)'Image & "," & Beacon_Status'Last(1)'Image & " .. " & Beacon_Status'First(2)'Image & "," & Beacon_Status'Last(2)'Image );
-   
-   for Info of All_Sensors loop
-      Area( Info.Sensor.X, Info.Sensor.Y ) := Sensor;
-      Area( Info.Beacon.X, Info.Beacon.Y ) := Beacon;
-   end loop;
-   
-   for Info of All_Sensors loop
-      declare
-         Sens : Position := Info.Sensor;
-         Beac : Position := Info.Beacon;
-         Diff : Position := Sens-Beac;
-         Delt_X : Integer := abs Diff.X;
-         Delt_Y : Integer := abs Diff.Y;
-         Dist : Integer := abs Diff;
-      begin
-         Area(Sens.X,Sens.Y) := Sensor;
-         Area(Beac.X,Beac.Y) := Beacon;
-         --Put_Line( "  " & To_String(Sens) & ", " & To_String(Beac) & " --> " & Dist'Image );
-         for Y in Sens.Y-Dist .. Sens.Y+Dist loop
-            for X in Sens.X-Dist .. Sens.X+Dist loop
-               declare
-                  Pos : Position := ( X, Y );
-                  Pos_Diff : Position := Pos-Sens;
-                  Pos_Diff_Dist : Integer := abs Pos_Diff;
-               begin
-                  --Put_Line( To_String(Pos) & "-" & To_String(Sens) & " = " & To_String(Pos_Diff) & " (" & Pos_Diff_Dist'Image & ")" );
-                  if Area( X, Y ) /= Beacon and then (abs (Pos - Sens)) <= Dist then
-                     Beacon_Status(X,Y) := True;
-                  end if;
-               end;
-            end loop;
-         end loop;
-      end;
-   end loop;
+   function Definitely_Not_Beacon ( Pos : Position ) return Boolean is (Get_Content(Pos) /= Beacon and then (for some Info of All_Sensors => (abs (Pos-Info.Sensor)) <= (abs (Info.Beacon-Info.Sensor))));
 
 end Tunnels;
--- Day15/src/tunnels.ads
+++ Day15/src/tunnels.ads
@@ -7,15 +7,10 @@ package Tunnels is
 
    type Content is ( Empty, Sensor, Beacon );
    
-   function Get_Content ( X, Y : Integer ) return Content;
-   function Get_Content ( P : Position ) return Content is (Get_Content(P.X,P.Y));
+   function Get_Content ( Pos : Position ) return Content;
+   function Get_Content ( X, Y : Integer ) return Content is (Get_Content(Position'(X,Y)));
    
-   function Definitely_Not_Beacon ( X, Y : Integer ) return Boolean;
-   function Definitely_Not_Beacon ( P : Position ) return Boolean is (Definitely_Not_Beacon(P.X,P.Y));
-   
-private
-   
-   Area : array ( Min_Pos.X .. Max_Pos.X, Min_Pos.Y .. Max_Pos.Y ) of Content := ( others => ( others => Empty ) );
-   Beacon_Status : array ( Area'Range(1), Area'Range(2) ) of Boolean := ( others => ( others => False ) );
+   function Definitely_Not_Beacon ( Pos : Position ) return Boolean;
+   function Definitely_Not_Beacon ( X, Y : Integer ) return Boolean is (Definitely_Not_Beacon(Position'(X,Y)));
 
 end Tunnels;
